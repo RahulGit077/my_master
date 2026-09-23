b65a71bad9ce8722d1a2cb713e07a89714fed19b
RahulGit077 <rahulkeccsd@gmail.com>
2026-09-23 20:52:10 +0530

    First commit; testing api

---
+++ src/test/java/com/example/home_server/HomeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
